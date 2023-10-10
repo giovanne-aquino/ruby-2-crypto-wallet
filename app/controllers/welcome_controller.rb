c0199400f577c3e5315242b6bed29beb4559f70b
Giovanne Aquino <giovanne.aquino.clear@gmail.com>
2023-10-10 10:36:37 -0300

    change view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    @meu_nome = params[:nome]
+    @curso = params[:curso]
+    unless @curso
+        @curso = "rails"      
+    end
+  unless @meu_nome
+      @meu_nome = "fulano"      
+  end
+
   end
 end
