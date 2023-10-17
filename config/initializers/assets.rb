366439df4b2e9f2c426bba0075abb37f028e1940
Giovanne Aquino <giovanne.aquino.clear@gmail.com>
2023-10-17 08:02:05 -0300

    fix problems

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.precompile += %w(jquery.js)
 
-Rails.application.config.assets.precompile += %w(notify.js)                                               
+Rails.application.config.assets.precompile += %w(notify.js)          
+Rails.application.config.assets.precompile +=  %w(application.css)
